535e8d88555b953d8a20a1c10bb726c8a6545fba
oldjunyi <oldjunyi@163.com>
2016-12-22 20:36:06 +0800

    checkout src

---
+++ src/main/java/com/mmyzd/betterautojump/asm/NameMappings.java
@@ -0,0 +1,14 @@
+package com.mmyzd.betterautojump.asm;
+
+public class NameMappings {
+
+	public static final String buttonList = "field_146292_n";
+	public static final String initGui = "func_73866_w_";
+	public static final String actionPerformed = "func_146284_a";
+
+	public static final String autoJumpEnabled = "field_189811_cr";
+	public static final String updateAutoJump = "func_189810_i";
+	public static final String onUpdateWalkingPlayer = "func_175161_p";
+	public static final String isSneaking = "func_70093_af";
+
+}
